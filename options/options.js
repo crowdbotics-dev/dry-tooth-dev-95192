710efba984e5fe16386da2ed4431dbae6a442ca0
Crowdbotics <engineering@crowdbotics.com>
2023-06-27 11:47:00 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "dry-tooth-dev-95192",
+  url: "https://dry-tooth-dev-95192.botics.co",
+  api: "https://dry-tooth-dev-95192.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
